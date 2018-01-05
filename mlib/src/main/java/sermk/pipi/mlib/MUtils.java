728ead33175abf02a8bdd9051d2bdbe0a4058566
Evgeny Chormonov <en.chormonov@gmail.com>
2018-01-05 22:01:51 +0300

    IMPR: add restriction by large size of file when intent by byte array

--- mlib/src/main/java/sermk/pipi/mlib/MUtils.java
+++ mlib/src/main/java/sermk/pipi/mlib/MUtils.java
@@ -1,6 +1,7 @@
 package sermk.pipi.mlib;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,6 +11,9 @@ import java.io.IOException;
  */
 
 public final class MUtils {
+
+    final static int MAX_BYTES = 500000;
+    final static String ERORR_MAX_BYTES = "limit max bytes";
 
     static public byte[] getByteOfFile(@NonNull final String pathNonLocalFile){
 
@@ -21,6 +25,11 @@ public final class MUtils {
         }
         catch (IOException e) {
             //e.printStackTrace();
+        }
+
+        if(bytes.length > MAX_BYTES){
+            bytes = ERORR_MAX_BYTES.getBytes();
+            Log.v(ERORR_MAX_BYTES, "!!!");
         }
 
         return bytes;
