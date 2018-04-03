66edd033e95f296730e09300a214b03ba54ed677
Evgeny Chormonov <en.chormonov@gmail.com>
2018-04-03 15:46:31 +0300

    TEST: add test save store settings

---
+++ app/src/androidTest/java/sermk/pipi/mclient/MCSettingsTest.java
@@ -0,0 +1,37 @@
+package sermk.pipi.mclient;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.google.gson.Gson;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by ser on 03.04.18.
+ */
+public class MCSettingsTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void saveMCSettingInstance() throws Exception {
+        MCSettings.Store tmp = MCSettings.getMCSettingInstance(activityTestRule.getActivity());
+
+        String js = new Gson().toJson(tmp);
+
+        System.out.println(js);
+
+        boolean res = MCSettings.saveMCSettingInstance(activityTestRule.getActivity(), js);
+
+        assertTrue(res);
+
+
+
+
+    }
+}
